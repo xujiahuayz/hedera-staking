2d65a88aad32e6d86bbff5caa632e1ebfdbc9223
Jiahua Xu <xujiahuayz@gmail.com>
2022-07-25 10:52:52 +0100

    add script to correct data files

---
+++ script/remove-duplicate-lines.sh
@@ -0,0 +1,38 @@
+zcat transactions-1608666693.024181007-.jsonl.gz | awk '!x[$0]++' | gzip -c > new.jsonl.gz
+mv new.jsonl.gz transactions-1608666693.024181007-.jsonl.gz
+
+zcat transactions-1597216761.139764000-.jsonl.gz | awk '!x[$0]++' | gzip -c > new.jsonl.gz
+mv new.jsonl.gz transactions-1597216761.139764000-.jsonl.gz
+
+zcat transactions-1628199909.287171000-.jsonl.gz | awk '!x[$0]++' | gzip -c > new.jsonl.gz
+mv new.jsonl.gz transactions-1628199909.287171000-.jsonl.gz
+
+zcat transactions-1619482017.729845000-.jsonl.gz | awk '!x[$0]++' | gzip -c > new.jsonl.gz
+mv new.jsonl.gz transactions-1619482017.729845000-.jsonl.gz
+
+zcat transactions-1612814010.515330003-.jsonl.gz | awk '!x[$0]++' | gzip -c > new.jsonl.gz
+mv new.jsonl.gz transactions-1612814010.515330003-.jsonl.gz
+
+zcat transactions-1616088083.458753000-.jsonl.gz | awk '!x[$0]++' | gzip -c > new.jsonl.gz
+mv new.jsonl.gz transactions-1616088083.458753000-.jsonl.gz
+
+zcat transactions-1618572077.431096000-.jsonl.gz | awk '!x[$0]++' | gzip -c > new.jsonl.gz
+mv new.jsonl.gz transactions-1618572077.431096000-.jsonl.gz
+
+zcat transactions-1623117818.283661000-.jsonl.gz | awk '!x[$0]++' | gzip -c > new.jsonl.gz
+mv new.jsonl.gz transactions-1623117818.283661000-.jsonl.gz
+
+zcat transactions-1637496532.397393000-.jsonl.gz | awk '!x[$0]++' | gzip -c > new.jsonl.gz
+mv new.jsonl.gz transactions-1637496532.397393000-.jsonl.gz
+
+zcat transactions-1623914199.470216000-.jsonl.gz | awk '!x[$0]++' | gzip -c > new.jsonl.gz
+mv new.jsonl.gz transactions-1623914199.470216000-.jsonl.gz
+
+zcat transactions-1644371980.852576123-.jsonl.gz | awk '!x[$0]++' | gzip -c > new.jsonl.gz
+mv new.jsonl.gz transactions-1644371980.852576123-.jsonl.gz
+
+zcat transactions-1650445386.486332000-.jsonl.gz | awk '!x[$0]++' | gzip -c > new.jsonl.gz
+mv new.jsonl.gz transactions-1650445386.486332000-.jsonl.gz
+
+zcat transactions-1632963998.091358000-.jsonl.gz | awk '!x[$0]++' | gzip -c > new.jsonl.gz
+mv new.jsonl.gz transactions-1632963998.091358000-.jsonl.gz
